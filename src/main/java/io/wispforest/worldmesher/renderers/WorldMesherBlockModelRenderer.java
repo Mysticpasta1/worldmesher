54a7805f1029a19981cff7234534acf4843f8b88
Mysticpasta1 <ethana5tc@outlook.com>
2024-03-06 09:02:24 -0600

    Update WorldMesherBlockModelRenderer.java

--- src/main/java/io/wispforest/worldmesher/renderers/WorldMesherBlockModelRenderer.java
+++ src/main/java/io/wispforest/worldmesher/renderers/WorldMesherBlockModelRenderer.java
@@ -3,6 +3,7 @@ package io.wispforest.worldmesher.renderers;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.block.BlockModelRenderer;
@@ -13,6 +14,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockRenderView;
+import net.minecraftforge.client.model.data.ModelData;
 
 import java.util.BitSet;
 import java.util.List;
@@ -40,7 +42,7 @@ public class WorldMesherBlockModelRenderer extends BlockModelRenderer {
     }
 
     @Override
-    public void renderSmooth(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrices, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay) {
+    public void tesselateWithAO(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrices, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, ModelData modelData, RenderLayer renderLayer) {
         float[] fs = new float[DIRECTIONS.length * 2];
         BitSet bitSet = new BitSet(3);
         BlockModelRenderer.AmbientOcclusionCalculator ambientOcclusionCalculator = new BlockModelRenderer.AmbientOcclusionCalculator();
@@ -62,10 +64,10 @@ public class WorldMesherBlockModelRenderer extends BlockModelRenderer {
         if (!quads.isEmpty()) {
             this.renderQuadsSmooth(world, state, pos, matrices, vertexConsumer, quads, fs, bitSet, ambientOcclusionCalculator, overlay);
         }
-
     }
 
-    public void renderFlat(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrices, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay) {
+    @Override
+    public void tesselateWithoutAO(BlockRenderView world, BakedModel model, BlockState state, BlockPos pos, MatrixStack matrices, VertexConsumer vertexConsumer, boolean cull, Random random, long seed, int overlay, ModelData modelData, RenderLayer renderLayer) {
         BitSet bitSet = new BitSet(3);
         BlockPos.Mutable mutable = pos.mutableCopy();
 
